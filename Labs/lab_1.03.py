dd2ed808b165c9967cb7374ae4a06631b6da0d61
Jerry (Jared) Price <jaredmillerprice@gmail.com>
2026-09-18 07:31:55 -0700

    change to bonus

--- Labs/lab_1.03.py
+++ Labs/lab_1.03.py
@@ -19,7 +19,10 @@
 #What would you like to wish for? more cats
 #Your wishes are cats, kittens, and more cats!
 
-#Bonus
+#Bonus 1: The Confused Genie
+#Have your genie print out the wishes in a mixed up order, different than how the user inputed their wishes in the console
+
+#Bonus 2
 #Can you add a new input that takes in a number that you can use to replicate a string
 #in the genie's final output that many times?
 
