0fa953de37834f3d25444938c3c170262a8c2b05
Jerry (Jared) Price <jaredmillerprice@gmail.com>
2026-09-23 09:38:46 -0700

    add quiz folder; add quiz1.py

---
+++ Quizzes/quiz1.py
@@ -0,0 +1,46 @@
+#Quiz 1
+#Name:
+
+#In this file, you will take a quiz concerning the content we have covered thus far in class!
+#You are welcome to use any previous Do Now or Lab files to help answer your questions
+
+#Please read the instructions for each problem carefully!
+
+#Question 1
+#With a comment, write your answer after the question.
+#Define and explain what data is in Python?
+
+#Question 2
+#With a comment, write your answer after the question.
+#Define and explain: what is a function? Give an example of a function we have used.
+
+#Question 3
+#With a comment, write your answer after the question
+#Define and explain: what is a variable? Give an example of a legal variable and an example of an illegal variable
+
+#Question 4
+#With a comment, write your answer after the question
+#Define and explain: what is casting?
+#1. When would I want to cast something?
+#2. Write an example of casting
+
+#Question 5
+#Write an example of concatenating a variable into a larger string.
+#Feel free to use whatever casting formatting you would like
+#Make sure to test the code to ensure that it works
+
+#Question 6
+#Debugging: uncomment the next four lines of code.
+#Run the program and interpret the error message
+#Make corrections to the code until you get the code properly running
+#Whenever you've made a successful correction, indicate so with a comment
+#HINT: you can write comments on the same line as code, so long as you write the comment
+#after the code with a #
+#Example
+#print('hello, world') #fixed function by closing paren.
+
+#Uncomment below!
+#favorite_number_str = input('What is your favorite number: ')
+#birth_month_str = input('What month were you born in: ')
+#lucky_number = favorite_number_str + birth_month_str
+#print('Your lucky number is ' + int(lucky_number)
