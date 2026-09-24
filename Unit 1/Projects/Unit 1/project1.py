5b153f355fe3895ffa5336a8dc0fe63f18806271
Jerry (Jared) Price <jaredmillerprice@gmail.com>
2026-09-24 09:54:55 -0700

    project 1 instructions

---
+++ Unit 1/Projects/Unit 1/project1.py
@@ -0,0 +1,23 @@
+#Project 1: MadLib
+#Name:
+
+#Overview
+#The purpose of this project is to put together a few key ideas we've learned thus far:
+#1. Inputs
+#2. Variables
+#3. Print
+#4. Concatenation
+
+#Project Requirements
+#1. The project must have a story with 10-15 words replaced as either: nouns, proper nouns, adverbs, adjectives, or amounts
+#2. The program must ask the user for inputs for your replaced words
+#3. After the user has responded to all inputs, the words must be placed into the original story
+#4. Your project should have an introduction that alerts the user to the MadLib, that they will be asked a number of prompts, etc.
+
+#Start your work below!
+#-----------------------------------------------
+
+#Variables
+
+#Story
+
